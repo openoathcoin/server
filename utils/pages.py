63d7fea18f2b84766e1f05ad40fc12830ce953eb
“openoathcoin” <openoathcoin@icloud.com>
2023-06-30 06:49:01 +0000

    create utils/pages.py

---
+++ utils/pages.py
@@ -0,0 +1,4 @@
+main_page_title = "One More Coin: A currency based on trust and transparency"
+main_page = "<|toggle|theme|>\n<center>\n<|navbar|>\n</center>"
+
+login_page = "## This is login page"
