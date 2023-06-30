537a5f29c99d5b07ca9dc5def277e65554fd9b0f
“openoathcoin” <openoathcoin@icloud.com>
2023-06-30 10:09:38 +0000

    delete utils/pages.py

--- utils/pages.py
+++
@@ -1,4 +0,0 @@
-main_page_title = "One More Coin: A currency based on trust and transparency"
-main_page = "<|toggle|theme|>\n<center>\n<|navbar|>\n</center>"
-
-login_page = "## This is login page"
