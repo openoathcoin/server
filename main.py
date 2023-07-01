8261e61509006af48b0f80331fd0128441faadb9
“openoathcoin” <openoathcoin@icloud.com>
2023-07-01 12:11:25 +0000

    update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 import taipy as tp
 from utils.pages.root_page import root_page
 from utils.pages.account_page import account_page
+from utils.pages.pay_page import pay_page
 
 # flask_app = Flask(__name__)
 
@@ -33,7 +34,8 @@ title = "One More Coin: A currency based on trust and transparency"  # text disp
 logo_file = "images/logo.jpg"  # icon displayed on browser tab
 
 pages = {"/": root_page,
-         "account": account_page}
+         "account": account_page,
+         "pay": pay_page}
 
 gui = tp.Gui(pages=pages)
 
@@ -41,10 +43,10 @@ if __name__ == "__main__":
   # for development
   # run source env/bin/activate to activate virtual environment
   # run python main.py in virtual environment
-  tp.run(gui, title=title, favicon=logo_file)  # listening at localhost:5000 by default
+  tp.run(gui, title=title, favicon=logo_file, watermark="")  # listening at localhost:5000 by default
 else:
   # for production
   # run source env/bin/activate to activate virtual environment
   # run gunicorn --bind localhost:5000 --worker-class gevent --worker-connections 1000 --workers $(nproc) --threads 1 main:app in virtual environment, adjust nproc and threads when more memory available
   # run uwsgi --http localhost:5000 --http-websockets --master --gevent 1000 --processes $(nproc) --threads 1 --module main:app in virtual environment, adjust nproc and threads when more memory available
-  app = tp.run(gui, title=title, favicon=logo_file, run_server=False)
+  app = tp.run(gui, title=title, favicon=logo_file, watermark="", run_server=False)
