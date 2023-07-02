27012ba30c1957c87f37b17caf70f3edf82debc1
openoathcoin <openoathcoin@icloud.com>
2023-07-03 00:56:56 +1000

    update root page

--- utils/pages/root_page.py
+++ utils/pages/root_page.py
@@ -1,8 +1,6 @@
 from taipy.gui import Markdown, navigate
 from datetime import datetime
 
-year = datetime.now().year
-
 root_page = Markdown("""
 <|layout|columns=1 auto 1|class_name=container align_columns_center|
 <|images/logo.jpg|image|width=55px|hover_text=One More Coin|on_action=handle_logo_click|>
@@ -11,14 +9,18 @@ root_page = Markdown("""
 
 <|toggle|theme|class_name=nolabel|>
 |>
+<br/><br/>
 
 <|content|>
+<br/><br/>
 
 <center>
-[Docs]()&emsp;[About]()&emsp;[Support]()&emsp;[Terms]()&emsp;[Privacy]()<br />
+[Docs]()&emsp;[About]()&emsp;[Support]()&emsp;[Terms]()&emsp;[Privacy]()<br/>
 <|© {year} One More Coin|text|>
 </center>
 """)
+
+year = datetime.now().year
 
 def handle_logo_click(state, id, action):
   navigate(state)
