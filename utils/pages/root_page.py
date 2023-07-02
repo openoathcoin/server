1654f49afb08e3fe08af6d536be8b5151659df9c
openoathcoin <openoathcoin@icloud.com>
2023-07-02 21:17:22 +1000

    update root page

--- utils/pages/root_page.py
+++ utils/pages/root_page.py
@@ -4,7 +4,7 @@ from datetime import datetime
 year = datetime.now().year
 
 root_page = Markdown("""
-<|layout|columns=1fr auto 1fr|class_name=container align_columns_center|
+<|layout|columns=1 auto 1|class_name=container align_columns_center|
 <|images/logo.jpg|image|width=55px|hover_text=One More Coin|on_action=handle_logo_click|>
 
 <|navbar|lov={[("/account", "Account"), ("/pay", "Pay")]}|>
@@ -15,7 +15,7 @@ root_page = Markdown("""
 <|content|>
 
 <center>
-[About]() &nbsp; &nbsp; &nbsp; [Support]() &nbsp; &nbsp; &nbsp; [Documentation]() &nbsp; &nbsp; &nbsp; [Terms]() &nbsp; &nbsp; &nbsp; [Privacy]()<br />
+[Docs]()&emsp;[About]()&emsp;[Support]()&emsp;[Terms]()&emsp;[Privacy]()<br />
 <|© {year} One More Coin|text|>
 </center>
 """)
