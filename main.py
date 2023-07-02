945bc6c8afe5bdddcdfa597ae416f69d0fbdada0
“openoathcoin” <openoathcoin@icloud.com>
2023-07-02 03:14:45 +0000

    update main.py

--- main.py
+++ main.py
@@ -43,10 +43,10 @@ if __name__ == "__main__":
   # for development
   # run source env/bin/activate to activate virtual environment
   # run python main.py in virtual environment
-  tp.run(gui, title=title, favicon=logo_file, watermark="")  # listening at localhost:5000 by default
+  tp.run(gui, host="localhost", port=8000, title=title, favicon=logo_file, watermark="")  # default host:port 127.0.0.1:5000
 else:
   # for production
   # run source env/bin/activate to activate virtual environment
-  # run gunicorn --bind localhost:5000 --worker-class gevent --worker-connections 1000 --workers $(nproc) --threads 1 main:app in virtual environment, adjust nproc and threads when more memory available
-  # run uwsgi --http localhost:5000 --http-websockets --master --gevent 1000 --processes $(nproc) --threads 1 --module main:app in virtual environment, adjust nproc and threads when more memory available
-  app = tp.run(gui, title=title, favicon=logo_file, watermark="", run_server=False)
+  # run gunicorn --bind localhost:8000 --worker-class gevent --worker-connections 1000 --workers $(nproc) --threads 1 main:app in virtual environment, adjust nproc and threads when more memory available
+  # run uwsgi --http localhost:8000 --http-websockets --master --gevent 1000 --processes $(nproc) --threads 1 --module main:app in virtual environment, adjust nproc and threads when more memory available
+  app = tp.run(gui, host="localhost", port=8000, title=title, favicon=logo_file, watermark="", run_server=False)
