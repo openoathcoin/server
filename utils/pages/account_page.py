a0d9f8e21abcf6fd26f14138cae18a2b17076f25
openoathcoin <openoathcoin@icloud.com>
2023-07-05 20:17:20 +1000

    update account page

--- utils/pages/account_page.py
+++ utils/pages/account_page.py
@@ -3,8 +3,7 @@ from taipy.gui import Markdown
 account_page = Markdown("""
 <|part|render={show_login}|
 <center>
-<|{username}|input|label=Username|><br/>
-<|{password}|input|password=True|label=Password|><br/><br/>
+<|{password}|input|password=True|label=Access token|><br/><br/>
 <|Log in to GitHub account|button|class_name=plain|><br/>
 or<br/>
 [CREATE GITHUB ACCOUNT](https://github.com/signup?source=login)
