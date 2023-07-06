62c1dc95c0cd4b3bf4e194c3d86a23fbedd5c8c7
openoathcoin <openoathcoin@icloud.com>
2023-07-07 02:55:42 +1000

    update root page

--- utils/pages/root_page.py
+++ utils/pages/root_page.py
@@ -2,12 +2,12 @@ from taipy.gui import Markdown, navigate
 from datetime import datetime
 
 root_page = Markdown("""
-<|layout|columns=1 auto 1|class_name=mb2|
-<|images/logo.jpg|image|width=50px|hover_text=One More Coin|on_action=handle_logo_click|>
+<|layout|columns=auto 1 auto|class_name=mb2|
+<|images/logo.jpg|image|width=40px|hover_text=One More Coin|on_action=handle_logo_click|>
 
 <|navbar|lov={[("/account", "Account"), ("/pay", "Pay")]}|class_name=fullheight|>
 
-<|toggle|theme|class_name=nolabel|>
+<|{orgname}|input|label=Currency|>
 |>
 
 <|part|class_name=mb2|
