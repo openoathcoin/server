9c3c43a2a5c257b1886336fa08c9587296c3d621
openoathcoin <openoathcoin@icloud.com>
2023-07-08 21:50:46 +1000

    update main.py

--- main.py
+++ main.py
@@ -44,11 +44,16 @@ if __name__ == "__main__":
   # for development
   # run source env/bin/activate to activate virtual environment
   # run python main.py in virtual environment
-  tp.run(gui, host="localhost", port=8000, title=title, favicon=logo_file, watermark="", debug=True, use_reloader=True)  # default host:port 127.0.0.1:5000
+  
+  # tp.run(gui, host="localhost", port=8000, title=title, favicon=logo_file, watermark="", debug=True, use_reloader=True)  # default host:port 127.0.0.1:5000
   # tp.run(gui, host="localhost", port=8000, title=title, favicon=logo_file, watermark="")  # run in notebook
+  gui.run(host="localhost", port=8000, title=title, favicon=logo_file, watermark="", debug=True, use_reloader=True)  # default host:port 127.0.0.1:5000
+  # gui.run(host="localhost", port=8000, title=title, favicon=logo_file, watermark="")  # run in notebook
 else:
   # for production
   # run source env/bin/activate to activate virtual environment
   # run gunicorn --bind localhost:8000 --worker-class gevent --worker-connections 1000 --workers $(nproc) --threads 1 main:app in virtual environment, adjust nproc and threads when more memory available
   # run uwsgi --http localhost:8000 --http-websockets --master --gevent 1000 --processes $(nproc) --threads 1 --module main:app in virtual environment, adjust nproc and threads when more memory available
-  app = tp.run(gui, host="localhost", port=8000, title=title, favicon=logo_file, watermark="", run_server=False)
+
+  # app = tp.run(gui, host="localhost", port=8000, title=title, favicon=logo_file, watermark="", run_server=False)
+  app = gui.run(host="localhost", port=8000, title=title, favicon=logo_file, watermark="", run_server=False)
